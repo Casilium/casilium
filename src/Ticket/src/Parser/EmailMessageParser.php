ba172e0897f24876594d4b406cb25d7cfb6c0ecf
sheridans <sam@sheridan.co.uk>
2020-12-04 14:31:16 +0000

    Code cleanup

--- src/Ticket/src/Parser/EmailMessageParser.php
+++ src/Ticket/src/Parser/EmailMessageParser.php
@@ -13,6 +13,7 @@ use function is_string;
 use function preg_match_all;
 use function preg_replace;
 use function sprintf;
+use function strcasecmp;
 use function strip_tags;
 use function strrpos;
 use function strtok;
@@ -63,7 +64,7 @@ class EmailMessageParser
                         gettype($contentType)
                     ));
                 }
-                if (strtok($contentType, ';') == $type) {
+                if (strcasecmp(strtok($contentType, ';'), $type) === 0) {
                     if (self::isBase64Encoded($part)) {
                         $body = base64_decode($part->getContent(), true);
                     } else {
@@ -97,7 +98,7 @@ class EmailMessageParser
     public static function isBase64Encoded(Message $part)
     {
         $encoding = $part->getHeaderField('ContentTransferEncoding') ?? null;
-        if (is_string($encoding) && $encoding == 'base64') {
+        if (is_string($encoding) && strcasecmp($encoding, 'base64') === 0) {
             return true;
         }
         return false;
@@ -106,6 +107,9 @@ class EmailMessageParser
     /**
      * Extract e-mail address from recipient string (ie "Test User" <test@example.com) will return
      * "test@example.com"
+     *
+     * @param string $string to extract from
+     * @return string|null extracted e-mail address or null
      */
     public static function getEmail(string $string): ?string
     {
@@ -114,7 +118,7 @@ class EmailMessageParser
         preg_match_all($pattern, $string, $matches);
 
         $email = $matches[1][0] ?? $string;
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) != false) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
             return $email;
         }
         return null;
@@ -122,6 +126,9 @@ class EmailMessageParser
 
     /**
      * Strip multiple new lines
+     *
+     * @param string $string string to parse
+     * @return string parsed string
      */
     public static function sanitiseLineBreaks(string $string): string
     {
@@ -131,6 +138,7 @@ class EmailMessageParser
     /**
      * Strip signature
      *
+     * @param string $body string to parse
      * @return string|string[]|null
      */
     public static function stripSignature(string $body): string
@@ -143,10 +151,16 @@ class EmailMessageParser
         return $body;
     }
 
+    /**
+     * Strip images from email
+     *
+     * @param string $body string to parse
+     * @return string parsed string
+     */
     public static function stripImages(string $body): string
     {
         $body = preg_replace('/<img\b[^>]*\bsrc\s*=\s*[\'"]cid[^>]*>/im', '', $body);
-        $body = preg_replace('/\[cid\:.*\]/im', '', $body);
+        $body = preg_replace('/\[cid:.*]/im', '', $body);
         return $body;
     }
 }
