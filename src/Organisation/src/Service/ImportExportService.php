1d8a30bff10a50f02d352e84159b30731ee50b47
Sam Sheridan <sam@sheridan.co.uk>
2024-01-18 02:00:19 +0000

    Updated deprecated doctrine calls

--- src/Organisation/src/Service/ImportExportService.php
+++ src/Organisation/src/Service/ImportExportService.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace Organisation\Service;
 
 use Doctrine\Dbal\Connection;
+use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
+
 use function array_keys;
 
 class ImportExportService
 {
-    /** @var EntityManagerInterface */
-    protected $entityManager;
+    protected EntityManagerInterface $entityManager;
 
-    /** @var Connection */
-    protected $connection;
+    protected Connection $connection;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -22,12 +22,15 @@ class ImportExportService
         $this->connection    = $entityManager->getConnection();
     }
 
+    /**
+     * @throws Exception
+     */
     public function fetchOrganisations(): array
     {
-        $sql   = 'SELECT * FROM organisation';
-        $query = $this->connection->prepare($sql);
+        $sql  = 'SELECT * FROM organisation';
+        $stmt = $this->connection->prepare($sql);
 
-        $query->execute();
+        $query  = $stmt->executeQuery();
         $result = $query->fetchAllAssociative();
 
         $header = array_keys($result[0]);
