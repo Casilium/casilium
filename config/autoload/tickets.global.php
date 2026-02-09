e77593fa4233ddb574f5670e6a62a08131897e1b
sheridans <sheridans@users.noreply.github.com>
2026-02-09 15:07:02 +0000

    fix: show msg on response email

--- config/autoload/tickets.global.php
+++ config/autoload/tickets.global.php
@@ -6,5 +6,9 @@ return [
     'tickets' => [
         // Number of days after resolution before tickets are auto-closed
         'auto_close_days' => 2,
+        'csat'            => [
+            'enabled'  => false,
+            'base_url' => 'https://example.com/customer-satisfaction',
+        ],
     ],
 ];
