82aa3cf00da6f0f644c8d2d3f1bfeeef69fe9fb5
sheridans <sam@sheridan.co.uk>
2026-02-24 22:41:27 +0000

    chore: code cleanup for CountUser command

--- src/User/src/Command/CountUser.php
+++ src/User/src/Command/CountUser.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace User\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Override;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,15 +21,17 @@ class CountUser extends Command
         $this->entityManager = $entityManager;
     }
 
+    #[Override]
     public function configure(): void
     {
         $this->setName('user:count')
             ->setDescription('Count users');
     }
 
+    #[Override]
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $count = (int) $this->entityManager->getRepository(User::class)->count([]);
+        $count = $this->entityManager->getRepository(User::class)->count([]);
         $output->writeln((string) $count);
         return 0;
     }
